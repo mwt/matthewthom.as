29120a4360f3369cc10f197c778085d7f0db8e8c
Matthew Thomas <matthew@mwt.me>
2021-03-07 14:39:50 -0600

    don't spam the log with wget progress

--- _bash/install.sh
+++ _bash/install.sh
@@ -6,7 +6,6 @@ yum install wget
 VTEX_VERSION=0.1
 
 echo "Download vtex"
-wget vtex.tar.gz "https://github.com/mwt/vtex/releases/download/v$VTEX_VERSION/vtex.tar.gz"
-
+wget -O vtex.tar.gz -nv "https://github.com/mwt/vtex/releases/download/v0.1/vtex.tar.gz"
 echo "Extract vtex"
 tar xzf vtex.tar.gz
