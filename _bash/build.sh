df511ca05ed2d1ce5784b9d041bb5a796f5e2bbd
Matthew Thomas <matthew@mwt.me>
2021-03-07 14:31:13 -0600

    build resume with vercel

---
+++ _bash/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+JEKYLL_ENV=production
+
+echo "Fetch variables from _config.yml"
+title=$(grep 'title:' _config.yml | tail -n1); title=${title#*: }
+bio=$(grep 'bio:' _config.yml | tail -n1); bio=${bio#*: }
+email=$(grep 'email:' _config.yml | tail -n1); email=${email#*: }
+github=$(grep 'github:' _config.yml | tail -n1); github=${github#*: }
+linkedin=$(grep 'linkedin:' _config.yml | tail -n1); linkedin=${linkedin#*: }
+
+echo "Build resume"
+./vtex/bin/x86_64-linux/pandoc resume.md -t latex --template="./_latex/resume-template.latex" --pdf-engine="./vtex/bin/x86_64-linux/pdflatex" -V title="$title" -V bio="$bio" -V email="$email" -V github="$github" -V linkedin="$linkedin" -o "./assets/resume.pdf"
+
+echo "Build website"
+jekyll build
--- _latex/pandoc.bash
+++
@@ -1,7 +0,0 @@
-title=$(grep 'title:' _config.yml | tail -n1); title=${title#*: }
-bio=$(grep 'bio:' _config.yml | tail -n1); bio=${bio#*: }
-email=$(grep 'email:' _config.yml | tail -n1); email=${email#*: }
-github=$(grep 'github:' _config.yml | tail -n1); github=${github#*: }
-linkedin=$(grep 'linkedin:' _config.yml | tail -n1); linkedin=${linkedin#*: }
-
-pandoc resume.md -t latex --template="./_latex/resume-template.latex" --pdf-engine=pdflatex -V title="$title" -V bio="$bio" -V email="$email" -V github="$github" -V linkedin="$linkedin" -o "./assets/resume.pdf"
