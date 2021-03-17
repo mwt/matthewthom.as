c1f8c06528a02dff3e10a0c15bb6f0d2985cf6b5
Matthew Thomas <matthew@mwt.me>
2021-03-17 12:50:00 -0500

    create proxy to github pages

--- _bash/build.sh
+++ _bash/build.sh
@@ -3,7 +3,7 @@
 JEKYLL_ENV=production
 
 echo "Fetch variables from _config.yml"
-title=$(grep 'title:' _config.yml | tail -n1); title=${title#*: }
+title="Matthew Wildrick Thomas"
 bio=$(grep 'bio:' _config.yml | tail -n1); bio=${bio#*: }
 email=$(grep 'email:' _config.yml | tail -n1); email=${email#*: }
 github=$(grep 'github:' _config.yml | tail -n1); github=${github#*: }
