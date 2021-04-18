a34cf91faac34e946f73b0b5445e98f2b89aceb2
Matthew Thomas <matthew@mwt.me>
2021-04-18 16:21:34 -0500

    move pdfs

--- _bash/build.sh
+++ _bash/build.sh
@@ -10,7 +10,7 @@ github=$(grep 'github:' _config.yml | tail -n1); github=${github#*: }
 linkedin=$(grep 'linkedin:' _config.yml | tail -n1); linkedin=${linkedin#*: }
 
 echo "Build resume"
-./vtex/bin/x86_64-linux/pandoc resume.md -t latex --template="./_latex/resume-template.latex" --pdf-engine="./vtex/bin/x86_64-linux/pdflatex" -V title="$title" -V bio="$bio" -V email="$email" -V github="$github" -V linkedin="$linkedin" -o "./assets/resume.pdf"
+./vtex/bin/x86_64-linux/pandoc resume.md -t latex --template="./_latex/resume-template.latex" --pdf-engine="./vtex/bin/x86_64-linux/pdflatex" -V title="$title" -V bio="$bio" -V email="$email" -V github="$github" -V linkedin="$linkedin" -o "./assets/pdfs/resume.pdf"
 
 echo "Build website"
 bundle exec jekyll build
