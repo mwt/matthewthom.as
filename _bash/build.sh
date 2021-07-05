21c1ff0f6f5bf3ea9b75d65c0aa5df4555e42923
Matthew Thomas <matthew@mwt.me>
2021-07-05 10:24:42 -0500

    replace resume with cv

--- _bash/build.sh
+++ _bash/build.sh
@@ -2,15 +2,15 @@
 
 JEKYLL_ENV=production
 
-echo "Fetch variables from _config.yml"
-title="Matthew Wildrick Thomas"
-bio=$(grep 'bio:' _config.yml | tail -n1); bio=${bio#*: }
-email=$(grep 'email:' _config.yml | tail -n1); email=${email#*: }
-github=$(grep 'github:' _config.yml | tail -n1); github=${github#*: }
-linkedin=$(grep 'linkedin:' _config.yml | tail -n1); linkedin=${linkedin#*: }
-
-echo "Build resume"
-./vtex/bin/x86_64-linux/pandoc resume.md -t latex --template="./_latex/resume-template.latex" --pdf-engine="./vtex/bin/x86_64-linux/pdflatex" -V title="$title" -V bio="$bio" -V email="$email" -V github="$github" -V linkedin="$linkedin" -o "./assets/pdfs/resume.pdf"
+# echo "Fetch variables from _config.yml"
+# title="Matthew Wildrick Thomas"
+# bio=$(grep 'bio:' _config.yml | tail -n1); bio=${bio#*: }
+# email=$(grep 'email:' _config.yml | tail -n1); email=${email#*: }
+# github=$(grep 'github:' _config.yml | tail -n1); github=${github#*: }
+# linkedin=$(grep 'linkedin:' _config.yml | tail -n1); linkedin=${linkedin#*: }
+# 
+# echo "Build resume"
+# ./vtex/bin/x86_64-linux/pandoc resume.md -t latex --template="./_latex/resume-template.latex" --pdf-engine="./vtex/bin/x86_64-linux/pdflatex" -V title="$title" -V bio="$bio" -V email="$email" -V github="$github" -V linkedin="$linkedin" -o "./assets/pdfs/resume.pdf"
 
 echo "Build website"
 bundle exec jekyll build
