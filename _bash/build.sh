17acf18c3b2897d17c6120702d676d2e153a59c4
Matthew W. Thomas <matthew@mwt.me>
2022-08-12 10:54:39 -0500

    automatic cache purge
    remove cache-buster

--- _bash/build.sh
+++ _bash/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+##
+# $1: URL to tar.gz file containing everything not in repo
+#
+##
 
 JEKYLL_ENV=production
 
@@ -8,9 +12,10 @@ JEKYLL_ENV=production
 # email=$(grep 'email:' _config.yml | tail -n1); email=${email#*: }
 # github=$(grep 'github:' _config.yml | tail -n1); github=${github#*: }
 # linkedin=$(grep 'linkedin:' _config.yml | tail -n1); linkedin=${linkedin#*: }
-# 
+#
 # echo "Build resume"
 # ./vtex/bin/x86_64-linux/pandoc resume.md -t latex --template="./_latex/resume-template.latex" --pdf-engine="./vtex/bin/x86_64-linux/pdflatex" -V title="$title" -V bio="$bio" -V email="$email" -V github="$github" -V linkedin="$linkedin" -o "./assets/pdfs/resume.pdf"
 
 echo "Build website"
-bundle exec jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
+curl -sS "$1" | tar -xzC _site
---
+++ _bash/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+##
+# $1: Api key for Buny.net
+#
+##
+
+PULL_ZONE="matthewthomas.b-cdn.net"
+
+# Create a tempfile
+#
+TMPFILE=$(mktemp /tmp/deploy.XXXXXX)
+
+# Deploy
+#
+# to endpoint
+rsync -rlci --log-file=$TMPFILE --delete _site/ endpoint:/
+# to bunny
+rclone sync _site/ bunny:/matthewthomas/
+
+# Purge the CDN using values from rsync log
+cat $TMPFILE | grep -E '\] (<fc)' | cut -d \  -f 5 | sed -e 's/index.html$//' | while read -r filename; do
+    echo "Purge https://$PULL_ZONE/$filename"
+    curl -sS --get --data-urlencode "url=https://$PULL_ZONE/$filename" --url 'https://api.bunny.net/purge' --header 'Accept: application/json' --header "AccessKey: $1"
+    echo ""
+done
