e2b2a8853bc80238d0ea2b147a85ddd19d4f8a6f
Matthew W. Thomas <matthew@mwt.me>
2022-08-14 12:30:46 -0500

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##
-# $1: Api key for Buny.net
+# $1: Api key for Bunny.net
 #
 ##
 
@@ -13,15 +13,19 @@ TMPFILE=$(mktemp /tmp/deploy.XXXXXX)
 # Deploy
 #
 # to endpoint for www (log is for CDN cache purge)
+echo "Copying www files to endpoint"
 rsync -rlci --log-file=$TMPFILE --delete _site/www/ endpoint:/www/
 # to endpoint for gemini (no cdn)
+echo "Copying gemini files to endpoint"
 rsync -rlci --delete _site/gemini/ endpoint:/gemini/
 # to bunny
+echo "Copying www files to Bunny Edge Storage"
 rclone sync _site/www/ bunny:/matthewthomas/
 
 # Purge the CDN using values from rsync log
+echo ""
+echo "Purging updated files from CDN:"
 cat $TMPFILE | grep -E '\] (<fc)' | cut -d \  -f 5 | sed -e 's/index.html$//' | while read -r filename; do
     echo "Purge https://$PULL_ZONE/$filename"
     curl -sS --get --data-urlencode "url=https://$PULL_ZONE/$filename" --url 'https://api.bunny.net/purge' --header 'Accept: application/json' --header "AccessKey: $1"
-    echo ""
 done
