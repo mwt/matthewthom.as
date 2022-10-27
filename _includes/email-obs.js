ade728c476c9c395d40c97d55cc4a73d5ed1582a
Matthew W. Thomas <matthew@mwt.me>
2022-10-27 13:03:35 -0500

    Update email-obs.js

--- _includes/email-obs.js
+++ _includes/email-obs.js
@@ -1,8 +1,6 @@
 /* trivial email obfuscation */
 var eme = "matthew";
 var eplace = "mwt.me";
-var xme = "matthew";
-var xplace = "mwt.me";
 
 /* replace social link a with string s  */
 function changesocial(a, s, pro) {
@@ -15,4 +13,3 @@ var elink = document.getElementById("mlink");
 
 /* replace email */
 changesocial(elink, `${eme}@${eplace}`, 'mailto');
-changesocial(xlink, `${xme}@${xplace}`, 'xmpp');
