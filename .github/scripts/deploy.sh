58b42611ef3087b09c1ab896c5ce12106fe80a84
Matthew W. Thomas <matthew@mwt.me>
2023-02-12 10:53:18 -0600

    disable deploy to bunny

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -20,17 +20,17 @@ rsync -rlci --log-file=$TMPFILE --delete --rsh=ssh _site/www/ endpoint::matthewt
 echo "Copying gemini files to endpoint"
 rsync -rlci --delete _site/gemini/ --rsh=ssh endpoint::matthewthomas/gemini/
 # to bunny
-echo "Copying www files to Bunny Edge Storage"
-lftp ftp://storage.bunnycdn.com << EOF
-user matthewthomas "$2"
-mirror --reverse --parallel=10 --delete --no-perms --transfer-all _site/www/ /matthewthomas/
-exit
-EOF
+#echo "Copying www files to Bunny Edge Storage"
+#lftp ftp://storage.bunnycdn.com << EOF
+#user matthewthomas "$2"
+#mirror --reverse --parallel=10 --delete --no-perms --transfer-all _site/www/ /matthewthomas/
+#exit
+#EOF
 
 # Purge the CDN using values from rsync log
-echo ""
-echo "Purging updated files from CDN:"
-cat $TMPFILE | grep -E '\] (<fc)' | cut -d \  -f 5 | sed -e 's/index.html$//' | while read -r filename; do
-    echo "Purge https://$PULL_ZONE/$filename"
-    curl -sS --get --data-urlencode "url=https://$PULL_ZONE/$filename" --url 'https://api.bunny.net/purge' --header 'Accept: application/json' --header "AccessKey: $1"
-done
+#echo ""
+#echo "Purging updated files from CDN:"
+#cat $TMPFILE | grep -E '\] (<fc)' | cut -d \  -f 5 | sed -e 's/index.html$//' | while read -r filename; do
+#    echo "Purge https://$PULL_ZONE/$filename"
+#    curl -sS --get --data-urlencode "url=https://$PULL_ZONE/$filename" --url 'https://api.bunny.net/purge' --header 'Accept: application/json' --header "AccessKey: $1"
+#done
