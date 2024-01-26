aec7e36965443f85ccb4a738bcc20291250048ae
Matthew W. Thomas <mthomas@u.northwestern.edu>
2024-01-26 18:04:03 +0000

    use updated version of vtex

--- vercel-install.sh
+++ vercel-install.sh
@@ -9,4 +9,4 @@
 NODE_ENV="production" npm install
 
 # Get and install vtex (for cv.pdf)
-curl -sSL "https://github.com/mwt/vtex/releases/download/v2.3/vtex.tar.gz" | tar -xzC "./"
+curl -sSL "https://github.com/mwt/vtex/releases/download/v2.4/vtex.tar.gz" | tar -xzC "./"
