ffc502354efd9f7518d6fd37b44af6ccc88a8e9e
Matthew Thomas <mthomas@ftc.gov>
2024-11-22 23:10:26 -0500

    Revert "hack around cv compile error"
    This reverts commit 8c8b46f747fba12b52383feca760929af404fb17.

--- vercel-build.sh
+++ vercel-build.sh
@@ -10,9 +10,9 @@ curl -sS "https://docs.matthewthom.as/website-fonts.tar.gz" | tar -xzC "./" &&
     npx @11ty/eleventy
 
 # Generate the cv.pdf
-#cd "./latex-temp"
-#../vtex/bin/x86_64-linux/lualatex -shell-escape -interaction nonstopmode -halt-on-error -file-line-error cv.tex
-#cd -
-#
-## Move the cv.pdf to the dist folder
-#mv "./latex-temp/cv.pdf" "./dist/assets/pdfs/cv.pdf" && echo "Moved cv.pdf to ./dist/assets/pdfs/"
+cd "./latex-temp"
+../vtex/bin/x86_64-linux/lualatex -shell-escape -interaction nonstopmode -halt-on-error -file-line-error cv.tex
+cd -
+
+# Move the cv.pdf to the dist folder
+mv "./latex-temp/cv.pdf" "./dist/assets/pdfs/cv.pdf" && echo "Moved cv.pdf to ./dist/assets/pdfs/"
